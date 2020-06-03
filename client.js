a6b62301c74b693e24141cb6f1cecb1df5b41610
odgerey <audreymedaino@gmail.com>
2020-06-03 19:01:10 +0000

    Snake is moving and initials are appearing

--- client.js
+++ client.js
@@ -14,7 +14,18 @@ const connect = function() {
   conn.on('connect', function() {
     console.log('Successfully connected to game server');
   });
-  
+
+ // conn.on('connect', function() {
+ //   conn.write("Name: AMT");
+ // });
+
+  conn.on('connect', function () {
+    conn.write("Name: AMT");
+    conn.write("Move: up");
+    setInterval(() => conn.write('Move: right'), 500);
+
+  });
+
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
